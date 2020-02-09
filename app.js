69e7ebc181aa7a0f7b2db48da0dc865e091167fd
Brian Burkard <brianburkard@gmail.com>
2020-02-08 23:10:05 -0500

    grid, helper functions

--- app.js
+++ app.js
@@ -1,14 +1,71 @@
+// get cell from coordinates
+// row = Number
+// col = Number
+const getCell = (row, col) => {
+    return document.querySelector(`[data-coord="${row},${col}"]`);
+}
+
+// get coordinate from cell element
+const getCoord = (element) => element.dataset.coord.split(",");
+
+// return cell with player1
+const getPlayer1 = () => document.querySelector('.player1');
+
+// cell = html element <td>
+// color = hex color, string, rgb
+const colorSquare = (cell, color) => {
+    cell.style.background = color;
+}
+
+//colorSquare(getCoor(4,4), 'red');
 const container = document.querySelector('.container');
 
-const tr = () => document.createElement('tr');
-const tableCell = () => document.createElement('td');
+// check for free movement
+const surroundings = (coordinates) => {
 
-for (let i = 0; i < 10; i++) {
-    const row = container.appendChild(tr());
-    row.classList.add(`row-${i}`);
-    console.log(row);
-    for(let j = 0; j < 10; j++) {
-        const tc = row.appendChild(tableCell());
-        tc.classList.add(`row-${i}-cell-${j}`);
+}
+
+// creates initial board
+const makeBoard = (rows,columns) => {
+    const tr = () => document.createElement('tr');
+    const tableCell = () => document.createElement('td');
+
+    for (let i = 0; i < rows; i++) {
+        const row = container.appendChild(tr());
+        for(let j = 0; j < columns; j++) {
+            const tc = row.appendChild(tableCell());
+            tc.dataset.coord = `${i},${j}`;
+        }
     }
 }
+
+// movement event listener
+document.addEventListener('keyup', (event) => {
+
+    const playerCoord = getCoord(getPlayer1());
+    console.log(playerCoord);
+
+    switch(event.key) {
+        case 'ArrowUp':
+            break;
+        case 'ArrowRight':
+            break;
+        case 'ArrowDown':
+            break;
+        case 'ArrowLeft':
+            break;
+        default:
+            return;
+    }
+});
+
+
+//////////////////////////////////////////
+///////////// Game Init //////////////////
+//////////////////////////////////////////
+
+makeBoard(10,10);
+const player1Start = getCell(4,4);
+//console.log(getCoord(player1Start));
+player1Start.classList.add('player1');
+getCell(4,5);
